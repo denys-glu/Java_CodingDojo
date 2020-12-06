07042141c8abdeaa018abe5c39085889c6865b09
denys_gl <oggyla@gmail.com>
2020-12-05 21:58:37 -0800

    importing files, calling objects methods.

---
+++ Fundamentals/Pythagorean/MainFile.java
@@ -0,0 +1,9 @@
+public class MainFile {
+  public static void main(String[] args) {
+
+    Pythagorean func = new Pythagorean();
+    Double result = func.calculateHypotenuse(4,4);
+    
+    System.out.println(result);
+  }
+}
